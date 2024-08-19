a3976184335b70e728205bc0a901b01f35b8102d
Animesh Bindal <98091040+Animesh-Bindal@users.noreply.github.com>
2024-08-19 10:14:30 +0530

    changesconn

--- db.js
+++ db.js
@@ -4,10 +4,7 @@ require('dotenv').config();
 const connectToDb = async () => {
     try {
         // await mongoose.connect('mongodb://127.0.0.1:27017/inotebook');
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-        });
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('Connection established successfully');
     }
     catch (error) {
